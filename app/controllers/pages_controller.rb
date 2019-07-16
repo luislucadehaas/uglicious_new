231c6a4b65a2460d811a2876695ae5561a863b88
Luis-Luca de Haas <luislucadehaas@gmail.com>
2019-07-16 13:34:17 +0200

    added controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,28 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def index
+    if params[:search].present? && params["search"]["query"] == ""
+      @products = Product.all
+    elsif params[:search].present?
+      sql_query = " \
+      products.title @@ :search \
+      OR products.description @@ :search \
+      "
+      @products = Product.where(sql_query, search: "%#{params["search"]["query"]}%")
+    else
+      @products = Product.all
+    end
+  end
+
+  private
+
+  def product_params
+
+    params.require(:product).permit(:title, :photo, :description, :price_in_cents, :quantity_in_kg, :min_quantity_to_order, :available_from, :available_until
+    )
+
+  end
 end
+
