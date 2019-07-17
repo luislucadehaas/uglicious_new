90bae3c1fc3ba4033976e460dc5ddf39184ff3b1
Luis-Luca de Haas <luislucadehaas@gmail.com>
2019-07-17 14:59:19 +0200

    added none saved files

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :products, only: [:new, :create, :edit, :update, :destroy] do
-resources :bookings, only: [:new, :create, :destroy, :update]
+  resources :products, only: [:new, :create, :edit, :update, :destroy, :index] do
+    resources :bookings, only: [:new, :create, :destroy, :update]
 end
    get 'farmer-dashboard', to: 'pages#dashboard_farmer', as: :dashboard_farmer
 
