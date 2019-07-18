fc8cae8be83f197dffc9fae7d4da2a70aa104d8e
Luis-Luca de Haas <luislucadehaas@gmail.com>
2019-07-18 17:58:00 +0200

    added files

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -34,32 +34,32 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
     @product.farm = current_user.farm
     if @product.save
-       redirect_to dashboard_farmer_path
-    else
-      render 'new'
-    end
+     redirect_to dashboard_farmer_path
+   else
+    render 'new'
   end
+end
 
-  def edit
-    @product = Product.find(params[:id])
-  end
+def edit
+  @product = Product.find(params[:id])
+end
 
-  def update
-    @product = Product.find(params[:id])
-    @product.update(product_params)
-    redirect_to dashboard_farmer_path
-  end
+def update
+  @product = Product.find(params[:id])
+  @product.update(product_params)
+  redirect_to dashboard_farmer_path
+end
 
-  def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
-    redirect_to dashboard_farmer_path
-  end
+def destroy
+  @product = Product.find(params[:id])
+  @product.destroy
+  redirect_to dashboard_farmer_path
+end
 
-  private
+private
 
-  def product_params
-    params.require(:product).permit(:title, :description, :price_in_cents, :quantity_in_kg, :min_quantity_to_order, :available_from, :available_until, :photo, :photo_cache, :subgroup_id)
-  end
+def product_params
+  params.require(:product).permit(:title, :description, :price_in_cents, :quantity_in_kg, :min_quantity_to_order, :available_from, :available_until, :photo, :photo_cache, :subgroup_id)
+end
 
 end
