cade1a2321450f390aa30e30548b4e657ae54522
Luis-Luca de Haas <luislucadehaas@gmail.com>
2019-07-23 13:28:27 +0200

    adding sweetalert2

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -5,7 +5,7 @@ class FarmsController < ApplicationController
         lat: farm.latitude,
         lng: farm.longitude,
         infoWindow: render_to_string(partial: "infowindow", locals: { farm: farm }),
-        image_url: helpers.asset_url('https://res.cloudinary.com/dc875ky15/image/upload/v1563810070/Gruppe_1_pfhknr.png')
+        image_url: helpers.asset_url('https://res.cloudinary.com/dc875ky15/image/upload/v1563869999/marker_wz5vh9.png')
       }
     end
   end
