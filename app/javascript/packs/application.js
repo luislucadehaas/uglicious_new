94dc34fd38be14e6550dd8a27177af47fb2616e0
Luis-Luca de Haas <luislucadehaas@gmail.com>
2019-07-23 16:56:11 +0200

    need to finish sweetalert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,45 +1,19 @@
 import "bootstrap";
-import "../plugins/flatpickr"
+import "../plugins/flatpickr";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import Swal from 'sweetalert2';
 
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
 
-
 import { initSweetalert } from '../plugins/init_sweetalert';
 
-const swalWithBootstrapButtons = Swal.mixin({
-  customClass: {
-    confirmButton: 'btn btn-success',
-    cancelButton: 'btn btn-danger'
-  },
-  buttonsStyling: false,
-})
+initSweetalert('#sweet-alert-demo', {
+  position: 'center',
+  type: 'success',
+  title: 'Thank you for your order',
+  showConfirmButton: false,
+  timer: 2000,
+});
 
-swalWithBootstrapButtons.fire({
-  title: 'Are you sure?',
-  text: "You won't be able to revert this!",
-  type: 'warning',
-  showCancelButton: true,
-  confirmButtonText: 'Yes, delete it!',
-  cancelButtonText: 'No, cancel!',
-  reverseButtons: true
-}).then((result) => {
-  if (result.value) {
-    swalWithBootstrapButtons.fire(
-      'Deleted!',
-      'Your file has been deleted.',
-      'success'
-    )
-  } else if (
-    // Read more about handling dismissals
-    result.dismiss === Swal.DismissReason.cancel
-  ) {
-    swalWithBootstrapButtons.fire(
-      'Cancelled',
-      'Your imaginary file is safe :)',
-      'error'
-    )
-  }
-})
--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -1,10 +1,12 @@
-import swal from 'sweetalert';
+import Swal from 'sweetalert2';
 
 const initSweetalert = (selector, options = {}) => {
   const swalButton = document.querySelector(selector);
+
+  console.log(swalButton);
   if (swalButton) { // protect other pages
     swalButton.addEventListener('click', () => {
-      swal(options);
+      Swal.fire(options);
     });
   }
 };
